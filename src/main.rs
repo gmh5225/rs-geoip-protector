3bfca5f64e92f5f3b546c6a84e2657abdbd42777
Your Name Here <your@email.example>
2023-10-17 12:18:14 +0800

    Print and get iso_code of ip parsed country

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+
+use maxminddb::geoip2;
+use std::net::IpAddr;
+use std::str::FromStr;
 fn main() {
-    println!("Hello, world!");
+
+    let reader = maxminddb::Reader::open_readfile("./GeoLite2-Country.mmdb").unwrap();
+
+    let ip: IpAddr = FromStr::from_str("113.57.107.91").unwrap();
+    let country: geoip2::Country = reader.lookup(ip).unwrap();
+    print!("country is {:?}", country.country.unwrap().iso_code.unwrap());
+
+
+
+    //
+    // country is "CN"
 }
